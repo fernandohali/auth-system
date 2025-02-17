62c87febda412cdc15f8e5f1e2bb7ac8be142046
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2025-02-17 11:50:42 -0300

    update

--- 3-extensionista/login.spec.cy.js
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:9000/login')
-    cy.get(':nth-child(2) > .form-control').type('extensionista@gmail.com')
-    cy.get(':nth-child(3) > .form-control').type('123123')
-    cy.get('.btn').click()
-  })
-})
---
+++ login.spec.cy.js
@@ -0,0 +1,5 @@
+describe("template spec", () => {
+  it("atraves da pagina da aplicacao com sucesso", () => {
+    cy.login(data.username, data.password);
+  });
+});
